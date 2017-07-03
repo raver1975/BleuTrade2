3a2b714a946740a8438952ba4c14dac75b8f35c1
Paul Klemstine <paulklemstine@gmail.com>
2017-07-03 09:35:59 -0500

    dang!

--- src/main/java/com/klemstinegroup/bleutrade/LocalDataListen.java
+++ src/main/java/com/klemstinegroup/bleutrade/LocalDataListen.java
@@ -151,7 +151,7 @@ public class LocalDataListen {
                                         System.out.println("FINAL: "+finalResult+"\tPREDICTION: " + prediction + "\t" + dateFormat.format(new Date()));
                                         try {
                                             PrintWriter pw = new PrintWriter(new FileWriter(new File("predictions.txt"), true));
-                                            pw.println(dateFormat.format("FINAL: "+finalResult+"\tPREDICTION: " + prediction + "\t" + dateFormat.format(new Date())));
+                                            pw.println("FINAL: "+finalResult+"\tPREDICTION: " + prediction + "\t" + dateFormat.format(new Date()));
                                             pw.close();
                                         } catch (IOException e) {
                                             e.printStackTrace();
